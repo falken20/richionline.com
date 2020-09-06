69b1b0701308c0a6f519697d309d6e32d9b04851
Richi <ricardorg20@gmail.com>
2020-09-06 20:24:53 +0200

    First commit

--- main.py
+++ main.py
@@ -16,18 +16,22 @@ load_dotenv(os.path.join(BASE_DIR, '.env'), override=True)
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     # url_for('static', filename='main.css')
     return render_template("main.html")
-    
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
+
 @app.route("/portfolio")
 def portfolio():
     return render_template("portfolio.html")
+
 
 @app.route('/latest-youtube-video')
 def lastest_video():
